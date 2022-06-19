882463f85c7386dd774f8e0d72dcb5a6774d9144
Karolina <karolina@example.com>
2022-06-19 20:34:31 +0200

    Blade template system

--- resources/views/doctors/list.blade.php
+++ resources/views/doctors/list.blade.php
@@ -1,33 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('template')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+@section('title') {{ $title }} @endsection
 
-    <title> {{ $title }} </title>
-</head>
-<body>
-
-
-<ul class="nav nav-pills">
-    <li class="nav-item">
-        <a class="nav-link active" href="doctors">Doctors</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">21</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">22</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">23</a>
-    </li>
-</ul>
-
+@section('content')
 <table class="table table-dark">
     <thead>
     <tr>
@@ -42,28 +17,16 @@
     </thead>
     <tbody>
     @foreach($doctorList as $doctor)
-    <tr>
-        <th scope="row">{{ $doctor['id'] }}</th>
-        <td>{{ $doctor['firstname'] }}</td>
-        <td>{{ $doctor['lastname'] }}</td>
-        <td>{{ $doctor['email'] }}</td>
-        <td>{{ $doctor['phone'] }}</td>
-        <td>{{ $doctor['address'] }}</td>
-        <td>{{ $doctor['status'] }}</td>
-    </tr>
+        <tr>
+            <th scope="row">{{ $doctor['id'] }}</th>
+            <td>{{ $doctor['firstname'] }}</td>
+            <td>{{ $doctor['lastname'] }}</td>
+            <td>{{ $doctor['email'] }}</td>
+            <td>{{ $doctor['phone'] }}</td>
+            <td>{{ $doctor['address'] }}</td>
+            <td>{{ $doctor['status'] }}</td>
+        </tr>
     @endforeach
     </tbody>
 </table>
-
-<footer class="text-center">
-    {{$footerYear}}
-</footer>
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-</html>
-
-
+@endsection('content')
